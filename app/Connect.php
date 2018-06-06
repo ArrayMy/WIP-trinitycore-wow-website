c46b18603e5af3bd3d39775163c30de30b3066ca
ArrayMy <34299302+ArrayMy@users.noreply.github.com>
2018-06-06 08:50:11 +0200

    Update Connect.php

--- app/Connect.php
+++ app/Connect.php
@@ -1,5 +1,17 @@
 <?php
-  require_once('Config.php');
+require_once('Configs.php');
+   class connect{
+     
+     public static $connect;
+     
+      public function connect(){
+       $Config = new Configs();
+       $Config->Load_Data();
+       self::connect = mysqli_connect($Config->self::config[Connect][Database_Host],$Config->self::config[Connect][Database_User],$Config->self::config[Connect][Database_User_Password]);
+      }
+     
+   }
+  
    
 
 
