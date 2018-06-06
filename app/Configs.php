b1da2831679127aaf48930696ae127da26aa965b
ArrayMy <34299302+ArrayMy@users.noreply.github.com>
2018-06-06 08:31:14 +0200

    Update Configs.php

--- app/Configs.php
+++ app/Configs.php
@@ -1,5 +1,5 @@
 <?
-
+require_once('Log.php');
    class Configs{
    
        public function __construct(){
@@ -7,12 +7,12 @@
            
        }
        
-       public function Check_Config_Exist(){
-           if(file_exist('../../configs/config.ini')){
+      public function Check_Config_Exist(){
+           if(file_exist("_DIR_ . \"/../../configs/config.ini")){
               $this->Check_Config_Data();   
            }else{
                  $this->Error_Msg = "Warning: File config.ini is not exist! Please rename default_config.ini on config.ini!";
-              if(file_exist('../../configs/default_config.ini')){
+              if(file_exist("_DIR_ . \"/../../configs/config.ini")){
                  $this->Check_Config_Data(); 
               }else{
                  $this->Error_Msg = "Error: File config.ini and default_config.ini are not exist!";
@@ -22,10 +22,12 @@
        
        public function Check_Config_Data(){
           if($this->Error_Msg[0]="W"){
-            echo "$this->Error_Msg";
+            $log = new log($this->Error_Msg);
+            $log->in_to_log();   
             $this->Bool = True;
           else if($this->Error_Msg[0]="E"){
-            echo "$this->Error_Msg";
+            $log = new log($this->Error_Msg);
+            $log->in_to_log();
             $this->Bool = False;
           }
        }
