bbab709625192d66aea0889d7b71df26d233da99
ArrayMy <34299302+ArrayMy@users.noreply.github.com>
2018-06-07 11:29:27 +0200

    Update Connect.php

--- app/Connect.php
+++ app/Connect.php
@@ -8,7 +8,7 @@ require_once('Log.php');
       public function connect(){
        $Config = new Configs();
        $Config->Load_Data();
-       self::connect = new mysqli($Config->self::config[Connect][Database_Host],$Config->self::config[Connect][Database_User],$Config->self::config[Connect][Database_User_Password]);
+       self::connect = new mysqli($Config::config[Connect][Database_Host],$Config::config[Connect][Database_User],$Config::config[Connect][Database_User_Password]);
        if(mysqli->connect_errno){
           $error = "Error: There was no connection to the database!"
           $check_error = new log($error);
