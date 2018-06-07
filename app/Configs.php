9f9df3f57c232cf6b33dae2e235709df2a8756f2
ArrayMy <34299302+ArrayMy@users.noreply.github.com>
2018-06-07 08:41:36 +0200

    Update Configs.php

--- app/Configs.php
+++ app/Configs.php
@@ -5,14 +5,12 @@ require_once('Log.php');
       public static $config;
       
        public function __construct(){
-           $this->Check_Config_Exist();
            
        }
        
       public function Check_Config_Exist(){
            if(file_exist("_DIR_ . \"/../../configs/config.ini")){
-              $this->Check_Config_Data();   
-           }else{
+           }else{  
                  $this->Error_Msg = "Warning: File config.ini is not exist! Please rename default_config.ini on config.ini!";
               if(file_exist("_DIR_ . \"/../../configs/config.ini")){
                  $this->Check_Config_Data(); 
@@ -21,30 +19,12 @@ require_once('Log.php');
               }
            }
        }
-       
-       public function Check_Config_Data(){
-          if($this->Error_Msg[0]="W"){
-            $log = new log($this->Error_Msg);
-            $log->in_to_log();   
-            $this->Bool = True;
-          else if($this->Error_Msg[0]="E"){
-            $log = new log($this->Error_Msg);
-            $log->in_to_log();
-            $this->Bool = False;
-            die();
-          }else{
-            $this->Bool = True;
-          }
-       }
-          
+    
        public function Load_Data(){
           $this->Check_Config_Exist;
-          if($this->Bool==True){
-             self::config = parse_ini_file("_DIR_ . \"/../../configs/config.ini",true);
-          }else if($this->Bool==False){
-             die();
-          }else{
-          }
+          $check_error = new log($this->Error_Msg);
+          $check_error->check_error();
+          self::config = parse_ini_file("_DIR_ . \"/../../configs/config.ini",true);
        }
    }
 
