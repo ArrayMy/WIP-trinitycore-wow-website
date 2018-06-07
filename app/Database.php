407f5b261f4dea79dc90ab503d1d78edf0ff4cda
ArrayMy <34299302+ArrayMy@users.noreply.github.com>
2018-06-07 11:35:48 +0200

    Update Database.php

--- app/Database.php
+++ app/Database.php
@@ -1,9 +1,22 @@
 <?php
 require_once('Log.php');
-require_once('Connect.php');  
+require_once('Connect.php');
+require_once('Configs');
+
   class database{
-      public function load_data(){
-         
+    
+      public function connect(){
+      $this->connect = new connect();
+      $this->connect->connect(); 
+      }
+      
+      public function load_data_from_config(){
+      $config = new Configs();
+      $config->Load_Data();
+      $config::$config[Databases][Database_World];
+      if(){
+        
+      }
       
       }
   }
